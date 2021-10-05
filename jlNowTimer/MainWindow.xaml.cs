567188c4245f54ca0c8ac5fabcbd5138f007447d
Jan Lægreid <jan@jl.no>
2021-10-06 01:55:21 +0200

    Add project files.

---
+++ jlNowTimer/MainWindow.xaml.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace jlNowTimer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        const int TIMER_INTERVAL = 1;
+        const int MAX_TIME = 60;
+        const int DEFAULT_START_TIME = 30;
+
+        private System.Windows.Threading.DispatcherTimer _minuteTimer = new System.Windows.Threading.DispatcherTimer();
+        private System.Windows.Threading.DispatcherTimer _seccondTimer = new System.Windows.Threading.DispatcherTimer();
+
+
+        private double _currentTimeValue = 30;
+        private double _timeDiskStep;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _timeDiskStep = 360 / (double)_currentTimeValue;
+
+            _minuteTimer.Tick += _minuteTimer_Tick;
+
+            _seccondTimer.Tick += (s, e) =>
+            {
+                //Every seccond:
+                //Alternate the visibility of the power led.
+                PowerLed.Visibility = (Visibility)Math.Abs((int)PowerLed.Visibility - 1);
+                //Move the timedisk one step.
+                TimeDisk.VisibleDegrees += _timeDiskStep;
+            };
+
+        }
+
+
+        private void _minuteTimer_Tick(object sender, EventArgs e)
+        {
+            _currentTimeValue -= 1;
+
+            if (_currentTimeValue == 0)
+            {
+                _minuteTimer.Stop();
+                _seccondTimer.Stop();
+                PowerLed.Visibility = Visibility.Hidden;
+                TimeDisk.VisibleDegrees += 360;
+
+
+                PlayTada();
+
+
+                //TODO:
+                // Her kan det legges inn forskjellig annet snax.
+                // Pause øvelser, ordtak, Haiku, 360-Tao, etc...
+                // Annet?
+            }
+
+            lblTime.Content = _currentTimeValue.ToString();
+            //TimeDisk.VisibleDegrees += _timeDiskStep;
+        }
+
+
+        private void PlayTada()
+        {
+            // Alarm
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            MediaPlayer player = new MediaPlayer();
+            player.Open(new Uri(@"file://" + System.IO.Path.Combine(baseDirectory, Properties.Settings.Default.wavFileName), UriKind.RelativeOrAbsolute));
+            player.Volume = 1f;
+            player.Play();
+            //player.Close();
+        }
+
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            _minuteTimer.Interval = TimeSpan.FromMinutes(TIMER_INTERVAL);
+            _seccondTimer.Interval = TimeSpan.FromSeconds(TIMER_INTERVAL);
+
+            lblTime.Content = DEFAULT_START_TIME.ToString();
+        }
+
+
+        private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            _minuteTimer.Stop();
+            _seccondTimer.Stop();
+            TimeDisk.VisibleDegrees = 0;
+            PowerLed.Visibility = Visibility.Hidden;
+
+            int currentTime = Int32.Parse(lblTime.Content.ToString());
+
+            if (e.Delta > 0) 
+            {
+                if (currentTime < MAX_TIME) currentTime += 1;
+                lblTime.Content = currentTime.ToString();
+                _currentTimeValue = currentTime;
+            }
+            else 
+            {
+                if (currentTime > 1) currentTime -= 1; // (!) there is now point in turning the timer down to 0 -> 1 is the lowest.
+                lblTime.Content = currentTime.ToString();
+                _currentTimeValue = currentTime;
+            }
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Make sure we can moove the time-disk window.
+            DragMove();
+
+            PlayTada();
+
+        }
+
+        private void Window_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (_minuteTimer.IsEnabled)
+            {
+                _minuteTimer.Stop();
+                _seccondTimer.Stop();
+                TimeDisk.VisibleDegrees = 0;
+                PowerLed.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                // Calculate how many steps the time disk must be divided up in when the disk ticks one step every second.:
+                // The disk is 360° -> divide it up in the amount of shoosen minutes times 60 seconds per minute.
+                _timeDiskStep = _currentTimeValue > 0 ? 360 / (_currentTimeValue * 60) : 0;
+                _minuteTimer.Start();
+                _seccondTimer.Start();
+            }
+        }
+
+        private void Window_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Configure open file dialog box
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "🎺🎉 Choose fanfare file.";
+            openFileDialog.Filter = "Sound files (.wav)|*.wav"; // Filter files by extension
+            openFileDialog.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Opens the open file dialog.
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Store the filePath (without the file name) into Property.settings so that
+                // the last opened folder is remembered.
+                Properties.Settings.Default.wavFileName = System.IO.Path.GetFileName(openFileDialog.FileName);
+                Properties.Settings.Default.Save();
+            }
+        }
+    }
+}
---
+++ jlNowTimer/TimeDisk.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace jlNowTimer
+{
+    internal class TimeDisk : Shape
+    {
+                
+        public double CentreX
+        {
+            get { return (double)GetValue(CentreXProperty); }
+            set { SetValue(CentreXProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for "MyProperty".  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CentreXProperty = DependencyProperty.Register("CentreXProperty", typeof(double), typeof(TimeDisk),
+            new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+
+        public double CentreY
+        {
+            get { return (double)GetValue(CentreYProperty); }
+            set { SetValue(CentreYProperty, value); }
+        }
+        public static readonly DependencyProperty CentreYProperty = DependencyProperty.Register("CentreYProperty", typeof(double), typeof(TimeDisk), 
+            new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+
+        public double Radius
+        {
+            get { return (double)GetValue(RadiusProperty); }
+            set { SetValue(RadiusProperty, value); }
+        }
+        public static readonly DependencyProperty RadiusProperty = DependencyProperty.Register("RadiusProperty", typeof(double), typeof(TimeDisk),
+            new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public double VisibleDegrees
+        {
+            get { return (double)GetValue(VisibleDegreesProperty); }
+            set
+            {
+                // Dersom vi havner på 360 eller mer, så bikker vi over startpunktet og begynner
+                // på ny runde. Da "nuller den ferdige runde seg ut". Det kan kanskje være greit
+                // i noen tilfeller, men ikke her. Derfor sørger vi for at om vi havner på 360 eller
+                // mer, så forblir vi på full disk.
+                if (value >= 360) value = 359.9999;
+                SetValue(VisibleDegreesProperty, value);
+            }
+        }
+        public static readonly DependencyProperty VisibleDegreesProperty = DependencyProperty.Register("VisibleDegreesProperty", typeof(double), typeof(TimeDisk),
+            new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public double RotationAngle
+        {
+            get { return (double)GetValue(RotationAngleProperty); }
+            set { SetValue(RotationAngleProperty, value); }
+        }
+        public static readonly DependencyProperty RotationAngleProperty = DependencyProperty.Register("RotationAngleProperty", typeof(double), typeof(TimeDisk),
+            new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+
+        // " Overrides "
+
+        protected override System.Windows.Media.Geometry DefiningGeometry
+        {
+            get
+            {
+                // / Create a StreamGeometry for describing the shape.
+                StreamGeometry geometry = new();
+                using ( StreamGeometryContext context = geometry.Open() )
+                {
+                    DrawGeometry(context);
+                }
+                geometry.Freeze();
+                return geometry;
+            }
+        }
+
+
+        // Methods
+
+        private void DrawGeometry(StreamGeometryContext context)
+        {
+            if (this.VisibleDegrees > 0)
+            {
+                Point startPoint = new Point(CentreX, CentreY);
+                Point outerArcStartPoint = CartesianCoordinatePoint(RotationAngle, Radius);
+                outerArcStartPoint.Offset(CentreX, CentreY);
+                Point outerArcEndPoint = CartesianCoordinatePoint(RotationAngle + VisibleDegrees, Radius);
+                outerArcEndPoint.Offset(CentreX, CentreY);
+                Size outerArcSize = new Size(Radius, Radius);
+
+                bool largeArc = VisibleDegrees > 180.0;
+
+                context.BeginFigure(startPoint, true, true);
+                context.LineTo(outerArcStartPoint, true, true);
+                context.ArcTo(outerArcEndPoint, outerArcSize, 0, largeArc, SweepDirection.Clockwise, true, true);
+                context.LineTo(startPoint, true, true);
+            }
+        } // DrawGeometry
+
+
+        private Point CartesianCoordinatePoint(double angle, double radius)
+        {
+            double angleRad = (Math.PI / 180.0) * (angle - 90);
+            double x = radius * Math.Cos(angleRad);
+            double y = radius * Math.Sin(angleRad);
+            return new Point(x, y);
+        } // CartesianCoordinatePoint
+
+    }
+}
